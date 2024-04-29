77424d98fca3dd9c7f7109caa38dd9f4a1da1148
gaginski <56768043+gaginski@users.noreply.github.com>
2024-04-29 16:33:33 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb://localhost:27017/TeamComposer';
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conectado ao banco de dados MongoDB');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  });
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API TeamComposer!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor na porta ${PORT}`);
+});
